f8bcc207e37a2ef8d7067250c8ec735f079bb0db
audreybernstein <audrey.bernstein@aol.com>
2018-08-10 14:16:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # dog.rb
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+end 
+s
+def name 
+  @name
+end
+
+def name= (new_name)
+  @name = new_name
+end 
+
+def breed
+  @breed
+end 
+
+def breed= (dog_breed)
+  @breed = dog_breed
+end
